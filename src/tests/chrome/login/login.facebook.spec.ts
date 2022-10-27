afee147db5747835b2836627fe2a80258b614e24
boulder-101 <boulder@101.inc>
2022-10-27 21:45:34 +0900

    feat: add wait functions and save class tc

---
+++ src/tests/chrome/login/login.facebook.spec.ts
@@ -0,0 +1,32 @@
+import Constant from '../../constant';
+import Common from '../../../utils';
+
+const { Browser } = require('selenium-webdriver');
+
+describe('chrome', () => {
+  let browser: any;
+  beforeAll(() => {
+    browser = new Common(Browser.CHROME);
+    browser.get(Constant.CLASS101_SUBSCRIPTION_URL_STAGING);
+  });
+
+  afterAll(() => {
+    browser.quit();
+  });
+
+  test('login with facebook', async () => {
+    await browser.clickElement(Constant.USER_NAVIGATION_LOGIN_BUTTON);
+
+    await browser.clickElement(Constant.SIGN_IN_WITH_OTHER_METHODS_BUTTON);
+
+    await browser.clickElement(Constant.LOGIN_PAGE_FACEBOOK_BUTTON);
+
+    await browser.fillInputElement(Constant.FACEBOOK_ID_FORM, Constant.FACEBOOK_USER_ID);
+
+    await browser.fillInputElement(Constant.FACEBOOK_PASSWORD_FORM, Constant.FACEBOOK_PASSWORD);
+
+    await browser.clickElement(Constant.FACEBOOK_LOGIN_BUTTON);
+
+    await browser.waitUntilVisible(Constant.USER_NAVIGATION_PROFILE_THUMBNAIL);
+  }, 60000);
+});
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -10,14 +10,43 @@ export class Common {
 
   async getElement(xPath: string) {
     await this.driver.wait(until.elementLocated(By.xpath(xPath)));
-    return this.driver.findElement(By.xpath(xPath));
+    const element = await this.driver.findElement(By.xpath(xPath));
+    return element;
   }
 
   get(url: string) {
-    return this.driver.get(url);
+    this.driver.get(url);
   }
 
   quit() {
     return this.driver.quit();
+  }
+
+  clickElement(xPath: string) {
+    this.getElement(xPath).then((element) => {
+      element.click();
+      console.log(`clicked: ${xPath}`);
+    });
+  }
+
+  fillInputElement(xPath:string, value:string) {
+    return this.getElement(xPath).then((element) => element.sendKeys(value));
+  }
+
+  async waitUntilVisible(xPath:string) {
+    console.log(`wait until visible: ${xPath}`);
+    await this.driver.wait(() => this.driver.findElements(By.xpath(xPath))
+      .then((elements:any) => elements.length !== 0));
+  }
+
+  async waitUntilNotVisible(xPath:string) {
+    console.log(`wait until not visible: ${xPath}`);
+    await this.driver.wait(() => this.driver.findElements(By.xpath(xPath))
+      .then((elements: any) => elements.length === 0));
+  }
+
+  async sleep(timeout: number) {
+    await this.driver.sleep(timeout);
+    console.log(`waiting...: ${timeout}`);
   }
 }
