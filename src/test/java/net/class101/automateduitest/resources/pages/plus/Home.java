7ab5dec8101c9a087ceedb41ed4b0ad6a90225da
Boulder(볼더) <99634272+boulder-101@users.noreply.github.com>
2023-02-07 15:16:40 +0900

    fix: webpl (#103)

--- src/test/java/net/class101/automateduitest/resources/pages/plus/Home.java
+++ src/test/java/net/class101/automateduitest/resources/pages/plus/Home.java
@@ -20,12 +20,12 @@ public class Home {
     private static final By USER_NAVIGATION_LOGIN_BUTTON = byXpath("(//p[text()='로그인'])[3]");
     private static final By TRENDING_CLASS_SECTION = withText("실시간 인기 클래스");
     private static final By FIRST_SAVE_BUTTON_ON_TRENDING_NOW = byXpath("(//section[contains(.,'실시간 인기 클래스')]//span[*[local-name()='svg']])[4]");
-    private static final By SAVED_CLASSES_SECTION_TITLE = byXpath("//h4[contains(.,'찜한 클래스')]");
+    private static final By SAVED_CLASSES_SECTION_TITLE = byXpath("//h2[contains(.,'찜한 클래스')]");
     private static final By RECOMMENDED_CLASS_SECTION = withText("추천 클래스");
     private static final By RECOMMENDED_CATEGORY_SECTION = byXpath("//*[text()='관심 클래스를 발견하지 못하셨나요?']");
     private static final By ENGLISH_CATEGORY_BUTTON = byXpath("//button[contains(.,'영어')]");
     private static final By FIRST_CURATION_ITEM = byXpath("//img[@alt='desktop-hero-banner']");
-    private static final By MY_CLASS_SECTION = byXpath("//h4[contains(.,'내 클래스')]");
+    private static final By MY_CLASS_SECTION = byXpath("//h2[contains(.,'내 클래스')]");
     private static final By SEARCH_BAR = byXpath("(//button[@type='button'])[9]");
     private static final By SEARCH_PANEL_TITLE = withText("지금 인기있는");
     private static final By SEARCH_BAR_INPUT_AFTER_CLICK = byTagName("input");
